9ccd66e5ba5c6e7374bfe87637d70fab454561fa
kaushinder <kaushinder@gmail.com>
2026-02-10 20:11:46 +0530

    Restaurant card are fetching data through swiggy restaurant api

--- practiceReact/src/components/Body.js
+++ practiceReact/src/components/Body.js
@@ -1,4 +1,328 @@
-import RestaurantCard from "./RestaurantCard";
+export const resObj = [
+  {
+    card: {
+      card: {
+        info: {
+          id: "677935",
+          name: "Goila Butter Chicken",
+          cloudinaryImageId: "5e19832da032dd69547565e27104706f",
+          cuisines: ["North Indian", "Biryani"],
+          avgRating: 4.1,
+          costForTwoMessage: "₹600 FOR TWO",
+          sla: { deliveryTime: 42, slaString: "40-45 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "906698",
+          name: "Food Fusion",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/18/08f9b352-da15-4e60-9190-bbee5db3a81a_906698.jpg",
+          cuisines: ["Indian"],
+          avgRating: 4.3,
+          costForTwoMessage: "₹300 FOR TWO",
+          sla: { deliveryTime: 36, slaString: "35-40 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "1021789",
+          name: "The Yellow Chilli",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/15/3c4373c5-68b4-403f-8350-a02d0b7e67f6_1021789.JPG",
+          cuisines: ["Indian", "Tandoor"],
+          avgRating: 4.3,
+          costForTwoMessage: "₹1600 FOR TWO",
+          sla: { deliveryTime: 41, slaString: "40-45 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "1020752",
+          name: "NH1 Bowls",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/4/41514dec-febb-4438-a160-a537e49bc4bd_1020752.jpg",
+          cuisines: ["Punjabi", "Home Food"],
+          avgRating: 4.7,
+          costForTwoMessage: "₹250 FOR TWO",
+          sla: { deliveryTime: 25, slaString: "20-30 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "798538",
+          name: "Nirula's",
+          cloudinaryImageId: "1bf86b1f0c2b12f58e6da57fb66ec36b",
+          cuisines: ["Ice Cream", "Fast Food"],
+          avgRating: 4.5,
+          costForTwoMessage: "₹250 FOR TWO",
+          sla: { deliveryTime: 33, slaString: "30-35 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "1005384",
+          name: "Di Rassoi Singh's",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/ed5b4edd-7658-424a-a306-adee069d6d4c_1005384.jpg",
+          cuisines: ["Fast Food"],
+          avgRating: 4.6,
+          costForTwoMessage: "₹299 FOR TWO",
+          sla: { deliveryTime: 52, slaString: "50-60 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "906704",
+          name: "Meals Wala",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/4eb2499d-9483-48b5-9ed5-689bcca30c2b_906704.jpg",
+          cuisines: ["Indian", "Snacks"],
+          avgRating: 3.6,
+          costForTwoMessage: "₹300 FOR TWO",
+          sla: { deliveryTime: 36, slaString: "35-40 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "568300",
+          name: "Pahalwan Dhaba",
+          cloudinaryImageId: "ua6eh8eaav4wh4efayjj",
+          cuisines: ["Indian", "Chinese"],
+          avgRating: 4.0,
+          costForTwoMessage: "₹300 FOR TWO",
+          sla: { deliveryTime: 35, slaString: "35-40 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "569255",
+          name: "Swad Punjab Da",
+          cloudinaryImageId: "vl7dgxuxoel8mau9nekp",
+          cuisines: ["Punjabi", "Snacks"],
+          avgRating: 4.2,
+          costForTwoMessage: "₹300 FOR TWO",
+          sla: { deliveryTime: 30, slaString: "30-35 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "568971",
+          name: "Parantha Factory",
+          cloudinaryImageId: "vhzteeidnhzdyokuiacm",
+          cuisines: ["North Indian"],
+          avgRating: 3.2,
+          costForTwoMessage: "₹300 FOR TWO",
+          sla: { deliveryTime: 35, slaString: "30-35 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "920409",
+          name: "Flame & Flavours",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/3/4323ae92-e2f2-46ef-a716-7c8838866b18_920409.jpg",
+          cuisines: ["Chinese", "Italian"],
+          avgRating: 4.1,
+          costForTwoMessage: "₹499 FOR TWO",
+          sla: { deliveryTime: 44, slaString: "40-45 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "1088478",
+          name: "Flame and Flavour Cafe",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/9/412c78ae-e1ed-45cf-94bb-a97bac7d7b13_1088478.jpg",
+          cuisines: ["North Indian"],
+          avgRating: 4.9,
+          costForTwoMessage: "₹250 FOR TWO",
+          sla: { deliveryTime: 46, slaString: "45-50 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "720691",
+          name: "Delhi Chaap Corner",
+          cloudinaryImageId: "ea3212fcaf96339f3bbfa7ddeb990cee",
+          cuisines: ["Indian", "Snacks"],
+          avgRating: 4.5,
+          costForTwoMessage: "₹300 FOR TWO",
+          sla: { deliveryTime: 40, slaString: "40-45 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "701931",
+          name: "Dil Se Delhi",
+          cloudinaryImageId: "f957fb3c3e4d89a01f6d0a525c8650ae",
+          cuisines: ["North Indian", "Chinese"],
+          avgRating: 3.6,
+          costForTwoMessage: "₹300 FOR TWO",
+          sla: { deliveryTime: 42, slaString: "40-45 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "1266543",
+          name: "The Biryani House",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2026/1/20/d595aa12-c9bd-44a6-b4e4-db093dcc14e6_1266543.jpg",
+          cuisines: ["Biryani"],
+          avgRating: 4.0,
+          costForTwoMessage: "₹199 FOR TWO",
+          sla: { deliveryTime: 36, slaString: "35-40 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "1309254",
+          name: "Indian Desire",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2026/1/13/bf6d9465-f8cf-4d50-9ce6-f50b24a674da_1309254.jpg",
+          cuisines: ["Tandoor", "Kebabs"],
+          avgRating: 5.0,
+          costForTwoMessage: "₹2000 FOR TWO",
+          sla: { deliveryTime: 38, slaString: "35-40 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "609705",
+          name: "UP 15",
+          cloudinaryImageId: "ahre5ckqj1vyzi9fsjbj",
+          cuisines: ["Indian", "Chinese"],
+          avgRating: 4.4,
+          costForTwoMessage: "₹300 FOR TWO",
+          sla: { deliveryTime: 106, slaString: "103-113 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "1260819",
+          name: "The Fit Feast",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2025/11/13/19611dbe-19d2-49b0-a6d3-16f54e2c0638_1260819.jpg",
+          cuisines: ["Healthy", "Chinese"],
+          avgRating: 4.2,
+          costForTwoMessage: "₹199 FOR TWO",
+          sla: { deliveryTime: 36, slaString: "35-40 MINS" },
+        },
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        info: {
+          id: "842826",
+          name: "FroGo Ice Cream",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/2/ceeab6e8-4c18-4c85-8d9c-72dde991ec22_842826.jpg",
+          cuisines: ["Ice Cream", "Desserts"],
+          avgRating: 5.0,
+          costForTwoMessage: "₹150 FOR TWO",
+          sla: { deliveryTime: 42, slaString: "40-45 MINS" },
+        },
+      },
+    },
+  },
+];
+
+const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+const RestaurantCard = ({ resObj }) => {
+  const info = resObj.card.card.info;
+
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src={CDN_URL + info.cloudinaryImageId}
+        alt={info.name}
+      />
+
+      <h3 className="res-name">{info.name}</h3>
+
+      <p className="res-rating">{info.avgRating} ⭐</p>
+
+      <p className="cuisines">{info.cuisines.join(", ")}</p>
+
+      <p className="res-sla">{info.sla.slaString}</p>
+
+      <p className="res-cost">{info.costForTwoMessage}</p>
+    </div>
+  );
+};
 
 const Body = () => {
   return (
@@ -9,22 +333,12 @@ const Body = () => {
       </div>
 
       <div className="res-container">
-        {/* Restaurant Card */}
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        {resObj.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant.card.card.info.id}
+            resObj={restaurant}
+          />
+        ))}
       </div>
     </div>
   );
